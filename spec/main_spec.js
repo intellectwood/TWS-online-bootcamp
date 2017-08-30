92b4b5a19599d30120644e96cee76f5a5a76a590
intellectwood <>
2017-08-30 04:12:03 +0000

    task11 finished!

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -1,39 +1,14 @@
+
+
 "use strict";
 
 var main = require("../lib/main.js");
 
 
-describe("With given ZIP code,outputing bar code.", function(){
+describe("xxx.", function(){
 
 
-    it("输入中国邮编 021000（内蒙古 > 海拉尔区 > 爱民路等）：", function(){
-        console.log('输入中国邮编 021000（内蒙古 > 海拉尔区 > 爱民路等）\n输出：');
-        var input='021000';
-        var result = main(input);
-        var expect_string ='| ||::: ::|:| :::|| ||::: ||::: ||::: |';
-        
-
-        expect(expect_string).toEqual(result);
-    });
-
-      
-    it("输入美国邮编 88595-0001(O BOX,EL PASO,El Paso,Texas)：", function(){
-        console.log('输入美国邮编 88595-0001(O BOX,EL PASO,El Paso,Texas)\n输出：');
-        var input='88595-0001';
-        var result = main(input);
-        var expect_string ="| |::|: |::|: :|:|: |:|:: :|:|: ||::: ||::: ||::: :::|| |";
-        
-        expect(expect_string).toEqual(result);
-    });
-    
-    it("输入美国条码邮编 "+'| |::|: |::|: :|:|: |:|:: :|:|: ||::: ||::: ||::: :::|| |'+"(O BOX,EL PASO,El Paso,Texas)：", function(){
-        console.log('输入美国邮编  | |::|: |::|: :|:|: |:|:: :|:|: ||::: ||::: ||::: :::|| | (O BOX,EL PASO,El Paso,Texas)\n输出：');
-        var input="| |::|: |::|: :|:|: |:|:: :|:|: ||::: ||::: ||::: :::|| |";
-        var result = main(input);
-        var expect_string ="88595-0001";
-        
-        expect(expect_string).toEqual(result);
-    }); 
+ 
 
     it("输入中国条码邮编 | ||::: ::|:| :::|| ||::: ||::: ||::: |（内蒙古 > 海拉尔区 > 爱民路等）：", function(){
         console.log("输入中国邮编 | ||::: ::|:| :::|| ||::: ||::: ||::: |（内蒙古 > 海拉尔区 > 爱民路等）\n输出：");
